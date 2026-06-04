0240aa838c0de6233b205f965a140bfa277892ef
prontmedfoa-cmd <prontmedfoa@gmail.com>
2026-06-04 11:45:01 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    connectionString: process.env.DATABASE_URL,
     max: 20,
-    idleTimeoutMillis: 30000
+    idleTimeoutMillis: 30000,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.on('error', (err) => {
@@ -16,3 +15,22 @@ pool.on('error', (err) => {
 });
 
 module.exports = pool;
+
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     max: 20,
+//     idleTimeoutMillis: 30000
+// });
+
+// pool.on('error', (err) => {
+//     console.error('Erro inesperado PostgreSQL:', err);
+// });
+
+// module.exports = pool;
